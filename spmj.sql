5bf07471362053a9556dde428b7946c3f2efb490
adeliatcp <58033181+adeliatcp@users.noreply.github.com>
2020-11-13 20:03:29 +0700

    Add files via upload
    update database

--- spmj.sql
+++ spmj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2020 at 12:58 PM
+-- Generation Time: Nov 13, 2020 at 02:02 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -38,8 +38,43 @@ CREATE TABLE `device` (
 --
 
 INSERT INTO `device` (`id`, `id_user`, `alamat`) VALUES
-('A1001', 3, 'Jl. Capung No. 25'),
-('A1004', 3, 'Jl. Bunga Anggrek No. 25');
+('A1001', 3, 'Jl. Cengkeh No. 29'),
+('A1004', 3, 'Jl. Bunga  No. 17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordering`
+--
+
+CREATE TABLE `ordering` (
+  `id` int(11) NOT NULL,
+  `id_device` varchar(5) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `telp` varchar(50) NOT NULL,
+  `id_pengepul` int(11) NOT NULL,
+  `harga` int(15) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ordering`
+--
+
+INSERT INTO `ordering` (`id`, `id_device`, `alamat`, `telp`, `id_pengepul`, `harga`, `status`) VALUES
+(12, 'A1004', 'Jl. Bunga Anggrek No. 21', '085734567890', 2, 36000, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `id_ordering` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -84,9 +119,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `name`, `password`, `email`, `telp`, `address`, `id_role`, `date`) VALUES
-(1, 'adelia', 'Adelia', 'aaaa', 'adeliatcp.pinem@gmail.com', '087845678291', 'Jl. Watumujur 1', 3, '2020-11-02'),
-(2, 'p_budi', 'Budi', '12345', 'budi@gmail.com', '085734567890', 'Jl. Bunga Anggrek No. 21', 2, '2020-11-02'),
-(3, 'tahuenak', 'Tahu Bulat Andi', 'tahuenak', 'tahubulat@gmail.com', '081223456789', 'Jl. Bunga Anggrek No. 21', 1, '2020-11-03');
+(1, 'adelia', 'Admin Adel', 'aaaa', 'adelia@gmail.com', '085723486790', 'Jl. Watumujur No.1', 3, '2020-11-02'),
+(2, 'budi', 'Budi', '12345', 'budi@gmail.com', '081398874567', 'Jl. Cimahi No. 5', 2, '2020-11-02'),
+(3, 'tahuenak', 'Tahu Bulat Enak', 'tahuenak', 'tahubulat@gmail.com', '081323456789', 'Jl. Bunga Anggrek No. 21', 1, '2020-11-03'),
+(4, 'supriadi', 'Supriadi', 'ssss', 'supriadi@gmail.com', '085734567890', 'Jl. Saxophone No. 3', 2, '2020-11-11');
 
 --
 -- Indexes for dumped tables
@@ -98,6 +134,21 @@ INSERT INTO `user` (`id`, `username`, `name`, `password`, `email`, `telp`, `addr
 ALTER TABLE `device`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_user` (`id_user`) USING BTREE;
+
+--
+-- Indexes for table `ordering`
+--
+ALTER TABLE `ordering`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_device` (`id_device`),
+  ADD KEY `id_pengepul` (`id_pengepul`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_device` (`id_ordering`);
 
 --
 -- Indexes for table `role`
@@ -116,6 +167,18 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `ordering`
+--
+ALTER TABLE `ordering`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
@@ -125,7 +188,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
